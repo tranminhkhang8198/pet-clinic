1e6396cf95fbd9c7573258de2c8ee1ee4a084495
Khang Tran <minhkhang811998@gmail.com>
2020-07-23 23:21:15 +0700

    Implement list all vets on vets index page

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,12 +1,11 @@
 package com.example.sfgpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.example.sfgpetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
-   protected Map<ID, T> map = new HashMap<>();
+import java.util.*;
+
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+   protected Map<Long, T> map = new HashMap<>();
 
    Set<T> findAll() {
       return new HashSet<>(map.values());
@@ -17,7 +16,13 @@ public abstract class AbstractMapService<T, ID> {
    }
 
    T save(ID id, T object) {
-      map.put(id, object);
+      if (object != null) {
+         if (object.getId() == null) {
+            object.setId(getNextId());
+         }
+
+         map.put(object.getId(), object);
+      }
 
       return object;
    }
@@ -28,5 +33,9 @@ public abstract class AbstractMapService<T, ID> {
 
    void delete(T object) {
       map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+   }
+
+   private Long getNextId() {
+      return Collections.max(map.keySet()) + 1;
    }
 }
--- pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/VetController.java
@@ -1,12 +1,29 @@
 package com.example.sfgpetclinic.controllers;
 
+import com.example.sfgpetclinic.model.Vet;
+import com.example.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+   private final VetService vetService;
+
+   public VetController(VetService vetService) {
+      this.vetService = vetService;
+   }
+
    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-   public String listOfVets() {
+   public String listOfVets(Model model) {
+      Vet vet3 = new Vet();
+      vet3.setId(1L);
+      vet3.setFirstName("Sam");
+      vet3.setLastName("Axe");
+
+      vetService.save(vet3);
+
+      model.addAttribute("vets", vetService.findAll());
       return "vets/index";
    }
 }
