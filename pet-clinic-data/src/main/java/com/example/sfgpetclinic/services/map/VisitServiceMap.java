33de456aacec16bfeea2b3758b5361f466880794
Khang Tran <minhkhang811998@gmail.com>
2020-07-25 17:29:55 +0700

    Implemented Map impl for Visit Service

---
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,38 @@
+package com.example.sfgpetclinic.services.map;
+
+import com.example.sfgpetclinic.model.Visit;
+import com.example.sfgpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+            || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
