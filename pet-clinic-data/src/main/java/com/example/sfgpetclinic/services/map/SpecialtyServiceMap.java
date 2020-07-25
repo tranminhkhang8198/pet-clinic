db65876684ac78ce460cc588004863411ba7d35d
Khang Tran <minhkhang811998@gmail.com>
2020-07-25 17:50:49 +0700

    Updated profiles for JPA service

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/SpecialtyServiceMap.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/SpecialtyServiceMap.java
@@ -2,11 +2,13 @@ package com.example.sfgpetclinic.services.map;
 
 import com.example.sfgpetclinic.model.Specialty;
 import com.example.sfgpetclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtyService {
     @Override
     public Set<Specialty> findAll() {
