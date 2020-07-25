a7e98871736ae592d9f2fd0c2d453fa726e417d1
Khang Tran <minhkhang811998@gmail.com>
2020-07-25 19:20:08 +0700

    Added project lombok

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Vet.java
@@ -1,9 +1,16 @@
 package com.example.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
@@ -11,12 +18,4 @@ public class Vet extends Person {
    @JoinTable(name = "vet_specialty", joinColumns = @JoinColumn(name = "vet_id"),
       inverseJoinColumns = @JoinColumn(name = "specialty_id"))
    private Set<Specialty> specialities = new HashSet<>();
-
-   public Set<Specialty> getSpecialities() {
-      return specialities;
-   }
-
-   public void setSpecialities(Set<Specialty> specialities) {
-      this.specialities = specialities;
-   }
 }
