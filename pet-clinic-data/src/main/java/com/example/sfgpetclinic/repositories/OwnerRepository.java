a5b557031d8d6add6fa63667cb32bb82f6dfb652
Khang Tran <minhkhang811998@gmail.com>
2020-07-25 16:22:18 +0700

    Added Spring Data JPA Repositories

---
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/repositories/OwnerRepository.java
@@ -0,0 +1,7 @@
+package com.example.sfgpetclinic.repositories;
+
+import com.example.sfgpetclinic.model.Owner;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+}
